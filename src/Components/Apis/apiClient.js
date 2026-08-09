953a04590cda428475f7e329c31dde081a0b7346
ali_mohamed <ali.mohamed.work20@gmail.com>
2026-08-10 01:08:41 +0300

    update url api

--- src/Components/Apis/apiClient.js
+++ src/Components/Apis/apiClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { getSecureCookie } from "../Helper/cookieUtils";
 const apiClient = axios.create({
-  baseURL: "https://localhost:7173/api/",
+  baseURL: "https://redastore.somee.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
